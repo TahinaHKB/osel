90fb650d7c6f04586a038876e8ef8f4827772203
HKB <165798571+TahinaHKB@users.noreply.github.com>
2025-12-01 20:15:40 +0100

    Refactor BenefitsSection component structure

--- src/sections/Benefits.tsx
+++ src/sections/Benefits.tsx
@@ -56,7 +56,6 @@ const BenefitsSection: React.FC = () => {
   if (!loaded) return <Loading />;
 
   return (
-    <AnimatedSection direction="right">
       <section
         id="benefits"
         className="relative py-20 bg-cover bg-center bg-no-repeat"
@@ -94,8 +93,8 @@ const BenefitsSection: React.FC = () => {
           </div>
         </div>
       </section>
-    </AnimatedSection>
   );
 };
 
 export default BenefitsSection;
+
